5e1dcd6194b58a60d2e3488a2b447e0b21834bdc
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-05-31 23:05:09 +0200

    add tests for move set builder logic

---
+++ test/AmazingChessTest/PieceLogic/MoveSetBuilderTests.cs
@@ -0,0 +1,66 @@
+﻿using AmazingChess.Game.Constants;
+using AmazingChess.Game.PieceLogic;
+
+namespace AmazingChessTest.ConsoleChessGameInterfaceTests
+{
+    public class MoveSetBuilderTests
+    {
+        private WhitePawnMoveSetBuilder _builder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Arrange
+            _builder = new WhitePawnMoveSetBuilder();
+        }
+
+        [Test]
+        public void BuildDiagonalMoveLimit_SetsCorrectValues()
+        {
+            //Act
+            _builder.BuildDiagonalMoveLimit();
+            var moveSet = _builder.GetMoveSet();
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                //given that these builder methods are never called, make sure the corresponding properties are null
+                Assert.That(moveSet.HorizontalMoveLimit, Is.Null);
+                Assert.That(moveSet.VerticalMoveLimit, Is.Null);
+
+                //verify that the resulting move limit properties are populated correctly
+                Assert.That(moveSet.DiagonalMoveLimit.BoardDimension, Is.EqualTo(BoardDimension.Diagonal));
+                Assert.That(moveSet.DiagonalMoveLimit.UpperLimit, Is.EqualTo(1));
+                Assert.That(moveSet.DiagonalMoveLimit.LowerLimit, Is.EqualTo(1));
+                Assert.That(moveSet.DiagonalMoveLimit.IncrementalMoveLimit, Is.EqualTo(1));
+                Assert.That(moveSet.DiagonalMoveLimit.DecrementalMoveLimit, Is.EqualTo(0));
+                Assert.That(moveSet.DiagonalMoveLimit.CanCapture, Is.True);
+            });
+        }
+
+        [Test]
+        public void BuildHorizontalMoveLimit_SetsCorrectValues()
+        {
+            //Act
+            _builder.BuildHorizontalMoveLimit();
+            var moveSet = _builder.GetMoveSet();
+
+            //Assert
+            Assert.Multiple(() =>
+            {
+                //given that these builder methods are never called, make sure the corresponding properties are null
+                Assert.That(moveSet.DiagonalMoveLimit, Is.Null);
+                Assert.That(moveSet.VerticalMoveLimit, Is.Null);
+
+                //given that a pawn is unable to move horizontally, verify that the default move limit property values are correctly populated
+                Assert.That(moveSet.HorizontalMoveLimit.BoardDimension, Is.EqualTo(BoardDimension.Horizontal));
+                Assert.That(moveSet.HorizontalMoveLimit.UpperLimit, Is.EqualTo(0));
+                Assert.That(moveSet.HorizontalMoveLimit.LowerLimit, Is.EqualTo(0));
+                Assert.That(moveSet.HorizontalMoveLimit.IncrementalMoveLimit, Is.EqualTo(0));
+                Assert.That(moveSet.HorizontalMoveLimit.DecrementalMoveLimit, Is.EqualTo(0));
+                Assert.That(moveSet.HorizontalMoveLimit.CanCapture, Is.False);
+                Assert.That(moveSet.HorizontalMoveLimit.CanCombineWithOtherDimensions, Is.False);
+            });
+        }
+    }
+}
