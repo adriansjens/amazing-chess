8121e04138bc3b5457212c78704a8ce92049dfdd
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-05-31 09:53:27 +0200

    add game menu choices and responses

---
+++ src/AmazingChess/GameMenu/Constants/ConsoleChessGameMenuChoices.cs
@@ -0,0 +1,8 @@
+﻿namespace AmazingChess.GameMenu
+{
+    public class ConsoleChessGameMenuChoices
+    {
+        public const string NewGame = "1";
+        public const string Exit = "2";
+    }
+}
