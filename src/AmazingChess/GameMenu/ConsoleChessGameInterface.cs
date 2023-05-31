e45752069e486572eee3602a42f8bc55101174b0
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-05-31 09:53:01 +0200

    add game menu interface and implementation

---
+++ src/AmazingChess/GameMenu/ConsoleChessGameInterface.cs
@@ -0,0 +1,25 @@
+﻿namespace AmazingChess.GameMenu
+{
+    public class ConsoleChessGameInterface : IChessGameInterface
+    {
+        public ConsoleChessGameInterface()
+        {
+        
+        }
+
+        public void RenderMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMenuChoiceFromUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExitGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
