50a7eeca2938e276b80d5015197e4468c90b5f48
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-05-31 19:42:07 +0200

    add chess piece interface and implementation

---
+++ src/AmazingChess/Game/Pieces/ChessPiece.cs
@@ -0,0 +1,14 @@
+﻿using AmazingChess.Game.ChessPieceLogic;
+using AmazingChess.Game.Constants;
+using AmazingChess.Game.PieceLogic;
+
+namespace AmazingChess.Game.Pieces
+{
+    public class ChessPiece : IChessPiece
+    {
+        public PieceName Name { get; set; }
+        public ChessColor Color { get; set; }
+        public string UnicodeCharacter { get; set; }
+        public MoveSet MoveSet { get; set; }
+    }
+}
---
+++ src/AmazingChess/Game/Pieces/IChessPiece.cs
@@ -0,0 +1,15 @@
+﻿using AmazingChess.Game.Constants;
+using AmazingChess.Game.PieceLogic;
+
+namespace AmazingChess.Game.ChessPieceLogic
+{
+    //evaluate whether this interface is necessary
+
+    public interface IChessPiece
+    {
+        public PieceName Name { get; }
+        public ChessColor Color { get; }
+        public string UnicodeCharacter { get; }
+        public MoveSet MoveSet { get; }
+    }
+}
