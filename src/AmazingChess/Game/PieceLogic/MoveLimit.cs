853bcddd61d4dd6517ec768f7d4a587a34fc4ef7
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-05-31 19:41:37 +0200

    add move set and move limit implementations

---
+++ src/AmazingChess/Game/PieceLogic/MoveLimit.cs
@@ -0,0 +1,15 @@
+﻿using AmazingChess.Game.Constants;
+
+namespace AmazingChess.Game.PieceLogic
+{
+    public class MoveLimit
+    {
+        public BoardDimension BoardDimension { get; set; }
+        public int IncrementalMoveLimit { get; set; } = 0;
+        public int DecrementalMoveLimit { get; set; } = 0;
+        public int UpperLimit { get; set; } = 0;
+        public int LowerLimit { get; set; } = 0;
+        public bool CanCapture { get; set; } = true;
+        public bool CanCombineWithOtherDimensions { get; set; } = false;
+    }
+}
---
+++ src/AmazingChess/Game/PieceLogic/MoveSet.cs
@@ -0,0 +1,9 @@
+﻿namespace AmazingChess.Game.PieceLogic
+{
+    public class MoveSet
+    {
+        public MoveLimit VerticalMoveLimit { get; set; }
+        public MoveLimit HorizontalMoveLimit { get; set; }
+        public MoveLimit DiagonalMoveLimit { get; set; }
+    }
+}
