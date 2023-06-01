e9457a23ab29929e4a144dcf591c52d80cc2ed8a
Adrian <70015674+adriansjens@users.noreply.github.com>
2023-06-01 15:01:17 +0200

    add mapping models

---
+++ src/AmazingChess/Game/Models/UnicodeMapping.cs
@@ -0,0 +1,11 @@
+﻿using AmazingChess.Game.Constants;
+
+namespace AmazingChess.Game.Models
+{
+    public class UnicodeMapping
+    {
+        public ChessColor Color { get; set; }
+        public PieceName PieceName { get; set; }
+        public string UnicodeCharacter { get; set; }
+    }
+}
